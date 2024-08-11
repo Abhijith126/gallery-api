8bbb8175fcd8374156f1e61e9f89bbc0742cc731
Abhijith <akshay.abhijith@gmail.com>
2024-08-11 10:09:37 +0200

    Test Vercel again

--- api/router.ts
+++ api/router.ts
@@ -3,5 +3,6 @@ import { getAlbumDetails, getImageById } from "./controller";
 
 export const router = express.Router();
 
+router.get("/", (req, res) => res.send("API is running!"));
 router.get("/album", getAlbumDetails);
 router.get("/photo/:id", getImageById);
